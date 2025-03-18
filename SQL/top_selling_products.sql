0a562d621707aa245d45ae3271665d3f449cd7e8
Isis Abraham <isiscavalcantiabraham@gmail.com>
2025-03-18 15:55:11 -0400

    Update top_selling_products.sql

--- SQL/top_selling_products.sql
+++ SQL/top_selling_products.sql
@@ -1,1 +1,9 @@
+SELECT  
+    p.ProductName,  
+    SUM(s.SalesQuantity) AS TotalSold,  
+    SUM(s.SalesAmount) AS TotalRevenue  
+FROM FactSales s  
+JOIN DimProduct p ON s.ProductKey = p.ProductKey  
+GROUP BY p.ProductName  
+ORDER BY TotalRevenue DESC;
 
